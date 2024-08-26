ea9160c9dc72d493ea05343e82093e249de3ffb1
Felipe Bandeira <felipebqueiroz@hotmail.com>
2024-08-26 18:01:10 -0300

    Código Bdc

--- main.c
+++ main.c
@@ -4,7 +4,9 @@
 * Data: [24/08/2024]
 * Revisões:
 * - [24, 13:40] - [implementação da conversão para base 2]
-* - [26, 09:46] - implementação da conversão para base 8]
+* - [26, 09:46] - [implementação da conversão para base 8]
+* - [26, 11:05] - [implementação da conversão para base 16]
+* - [26, 16,52] - [implementação da conversão para o código bdc] 
 */
 
 #include <stdio.h>
@@ -47,9 +49,62 @@ void decimalParaBase8(int numero){
   printf("\n");
 }
 
+void decimalParaBase16(int numero) {
+  char hexaDecimal[32];
+  int i = 0;    
+  int resto;
+
+  printf("\nConversão Decimal -> Base 16:\n");
+  while (numero > 0) {
+    resto = numero % 16;
+    if (resto < 10) {
+      hexaDecimal[i] = resto + '0';          
+    }else{
+      hexaDecimal[i] = resto - 10 + 'A';  
+    }
+    printf("Dividindo %d por 16, Resto: %c\n", numero, hexaDecimal[i]);
+    numero = numero / 16;
+    i++;
+  }
+  
+  printf("Número base 16: ");
+  for (int j = i - 1; j >= 0; j--) {
+    printf("%c", hexaDecimal[j]);  
+  }
+  printf("\n");
+}
+
+void decimalParaBdc(int numero) {
+  int numeros[32];
+  int bdc[4];  
+  int i = 0;
+
+  while (numero > 0) {
+    numeros[i] = numero % 10;
+    numero /= 10;
+    i++;
+  }
+
+  printf("Representação BCD: ");
+  for (int j = i - 1; j >= 0; j--) {
+    int digito = numeros[j];
+
+    for (int k = 0; k < 4; k++) {
+      bdc[k] = digito % 2;
+      digito /= 2;
+    }
+
+    for (int k = 3; k >= 0; k--) {
+      printf("%d", bdc[k]);
+    }
+    printf(" ");
+  }
+  printf("\n");
+}
+
 int main() {
   int opcao, numero;
-  
+
   while (1) {
     printf("\nCalculadora Didática\n");
     printf("1 - Converter de Base 10 para Base 2\n");
@@ -75,6 +130,10 @@ int main() {
       decimalParaBinario(numero);
     }else if(opcao == 2){
       decimalParaBase8(numero);
+    }else if(opcao == 3){
+      decimalParaBase16(numero);
+    }else if(opcao == 4){
+      decimalParaBdc(numero);
     }
   }
   return 0;
